d13440e88553f173699a5569fbe7b92950850877
itinerare <itinerare@users.noreply.github.com>
2022-12-13 16:53:33 +0000

    refactor: fix blade formatting

--- resources/views/admin/masterlist/_approve_trade_modal.blade.php
+++ resources/views/admin/masterlist/_approve_trade_modal.blade.php
@@ -4,25 +4,25 @@
     <div class="row">
         <div class="col-md-3">
             <div class="form-group">
-                {!! Form::checkbox('is_giftable['. $character->id.']', 1, $character->is_giftable, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
-                {!! Form::label('is_giftable['. $character->id.']', 'Is Giftable', ['class' => 'form-check-label ml-3']) !!}
+                {!! Form::checkbox('is_giftable[' . $character->id . ']', 1, $character->is_giftable, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
+                {!! Form::label('is_giftable[' . $character->id . ']', 'Is Giftable', ['class' => 'form-check-label ml-3']) !!}
             </div>
         </div>
         <div class="col-md-3">
             <div class="form-group">
-                {!! Form::checkbox('is_tradeable['. $character->id.']', 1, $character->is_tradeable, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
-                {!! Form::label('is_tradeable['. $character->id.']', 'Is Tradeable', ['class' => 'form-check-label ml-3']) !!}
+                {!! Form::checkbox('is_tradeable[' . $character->id . ']', 1, $character->is_tradeable, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
+                {!! Form::label('is_tradeable[' . $character->id . ']', 'Is Tradeable', ['class' => 'form-check-label ml-3']) !!}
             </div>
         </div>
         <div class="col-md">
             <div class="row">
                 <div class="col-md-4 form-group">
-                    {!! Form::checkbox('is_sellable['. $character->id.']', 1, $character->is_sellable, ['class' => 'form-check-input', 'data-toggle' => 'toggle', 'id' => 'resellable']) !!}
-                    {!! Form::label('is_sellable['. $character->id.']', 'Is Resellable', ['class' => 'form-check-label ml-3']) !!}
+                    {!! Form::checkbox('is_sellable[' . $character->id . ']', 1, $character->is_sellable, ['class' => 'form-check-input', 'data-toggle' => 'toggle', 'id' => 'resellable']) !!}
+                    {!! Form::label('is_sellable[' . $character->id . ']', 'Is Resellable', ['class' => 'form-check-label ml-3']) !!}
                 </div>
                 <div class="col-md d-flex">
                     {!! Form::label('Resale Value') !!} {!! add_help('This value is publicly displayed on the character\'s page.') !!}
-                    {!! Form::text('sale_value['. $character->id.']', $character->sale_value, ['class' => 'form-control']) !!}
+                    {!! Form::text('sale_value[' . $character->id . ']', $character->sale_value, ['class' => 'form-control']) !!}
                 </div>
             </div>
         </div>
